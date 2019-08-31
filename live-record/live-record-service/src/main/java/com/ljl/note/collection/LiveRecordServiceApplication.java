a765e275e914d764f8ae38c3a825efe954b29871
jinlei.li_ <jinlei.li@weimob.com>
2019-08-31 21:10:54 +0800

    添加 module

--- live-record/live-record-service/src/main/java/com/ljl/note/collection/LiveRecordServiceApplication.java
+++ live-record/live-record-service/src/main/java/com/ljl/note/collection/LiveRecordServiceApplication.java
@@ -1,8 +1,14 @@
 package com.ljl.note.collection;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource(locations = {
+		"classpath*:applicationContext-dubbo.xml"
+})
+@EnableDubboConfiguration
 @SpringBootApplication
 public class LiveRecordServiceApplication {
 
