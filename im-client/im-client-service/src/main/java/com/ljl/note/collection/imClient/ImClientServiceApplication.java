fab872c651b9ac0bca820b36c0628662832d301f
jinlei.li_ <liaura_ljl@163.com>
2019-10-25 09:04:11 +0800

    feat:add im-client module

---
+++ im-client/im-client-service/src/main/java/com/ljl/note/collection/imClient/ImClientServiceApplication.java
@@ -0,0 +1,29 @@
+package com.ljl.note.collection.imClient;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+
+@EnableDubboConfiguration
+@EnableAsync
+@ImportResource(locations = {
+		"classpath*:applicationContext-dubbo.xml"
+})
+@SpringBootApplication
+public class ImClientServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ImClientServiceApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImClientServiceApplication.class, args);
+	}
+
+}
---
+++ im-server/im-server-service/src/main/java/com/ljl/note/collection/imServer/ImServerServiceApplication.java
@@ -0,0 +1,30 @@
+package com.ljl.note.collection.imServer;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+@EnableDubboConfiguration
+@EnableAsync
+@ImportResource(locations = {
+		"classpath*:applicationContext-dubbo.xml"
+})
+@SpringBootApplication
+public class ImServerServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ImServerServiceApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImServerServiceApplication.class, args);
+	}
+
+}
